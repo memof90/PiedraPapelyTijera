f76ef3d07754544125d5db014030e7062a676703
memo figueredo <memof80@gmail.com>
2019-01-28 11:08:00 -0500

    se agregaron los conectores y las acciones

--- PiedraPapelyTijera/ViewController.swift
+++ PiedraPapelyTijera/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //conectores
+    @IBOutlet weak var turnoComputadoraLabel: UILabel!
+    
+    @IBOutlet weak var labelMensaje: UILabel!
+    
+    @IBOutlet weak var piedraBoton: UIButton!
+    
+    @IBOutlet weak var tijeraBoton: UIButton!
+    
+    @IBOutlet weak var papelBoton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+ 
+    //acciones
+    
+   
+    @IBAction func playPiedra(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func playTijera(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func playPapel(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func playListo(_ sender: Any) {
+        
+    }
 }
 

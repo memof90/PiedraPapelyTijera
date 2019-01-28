03786b7d0e2b57ef560d991033e4cb333d003870
memo figueredo <memof80@gmail.com>
2019-01-28 12:24:50 -0500

    se añadió una nueva vista llamada acercaViewController con el fin de controlar las instrucciones del juego

---
+++ PiedraPapelyTijera/AcercaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AcercaViewController.swift
+//  PiedraPapelyTijera
+//
+//  Created by Memo Figueredo on 1/28/19.
+//  Copyright © 2019 Memo Figueredo. All rights reserved.
+//
+
+import UIKit
+
+class AcercaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
